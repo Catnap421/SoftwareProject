8382f2890eb266a979cae9590541b79088a02b9a
catnap421 <wlq7410@naver.com>
2021-03-27 21:30:08 +0900

    Add: hw2

---
+++ assignments/hw2/Location.java
@@ -0,0 +1,51 @@
+package hw2;
+
+import hw1.Point3d;
+
+import java.util.Objects;
+
+/**
+ * This class represents a specific location in a 2D map.  Coordinates are
+ * integer values.
+ **/
+public class Location
+{
+    /** X coordinate of this location. **/
+    public int xCoord;
+
+    /** Y coordinate of this location. **/
+    public int yCoord;
+
+
+    /** Creates a new location with the specified integer coordinates. **/
+    public Location(int x, int y)
+    {
+        xCoord = x;
+        yCoord = y;
+    }
+
+    /** Creates a new location with coordinates (0, 0). **/
+    public Location()
+    {
+        this(0, 0);
+    }
+
+    /** Compare two locations for equality. */
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Location) {
+            Location other = (Location) obj;
+            if(xCoord == other.xCoord && yCoord == other.yCoord){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /** Overrides hashcode method **/
+    @Override
+    public int hashCode() {
+        return Objects.hash(xCoord, yCoord);
+    }
+}
